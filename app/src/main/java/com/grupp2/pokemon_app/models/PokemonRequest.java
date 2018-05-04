d9db9eecb8ffdb04314afadd768d2d9d87aaef0f
Oskar Wikström <oskar@Oskars-MacBook-Pro.local>
2018-05-04 12:40:05 +0200

    logcat get name retrofit

---
+++ app/src/main/java/com/grupp2/pokemon_app/models/PokemonRequest.java
@@ -0,0 +1,8 @@
+package com.grupp2.pokemon_app.models;
+
+/**
+ * Created by oskar on 2018-05-04.
+ */
+
+public class PokemonRequest {
+}
---
+++ app/src/main/java/com/grupp2/pokemon_app/pokeapi/PokeapiService.java
@@ -0,0 +1,8 @@
+package com.grupp2.pokemon_app.pokeapi;
+
+/**
+ * Created by oskar on 2018-05-04.
+ */
+
+public interface PokeapiService {
+}
