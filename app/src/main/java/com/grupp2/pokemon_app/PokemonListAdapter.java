8c9104a88a39ed566d224238d7dfa7543862397e
SW4GL0RD <o.sarholm@hotmail.com>
2018-05-06 14:08:09 +0200

    Det mesta är nu här, den hämtar alla pokémons och visar upp dem, däremot så fungerar inte bildlänken

---
+++ app/src/main/java/com/grupp2/pokemon_app/PokemonListAdapter.java
@@ -0,0 +1,67 @@
+package com.grupp2.pokemon_app;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.grupp2.pokemon_app.models.Pokemon;
+
+import java.util.ArrayList;
+
+public class PokemonListAdapter extends RecyclerView.Adapter<PokemonListAdapter.ViewHolder>{
+
+    private ArrayList<Pokemon> dataset;
+    private Context context;
+
+    public PokemonListAdapter(Context context){
+        this.context = context;
+        dataset = new ArrayList<>();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pokemon, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Pokemon p = dataset.get(position);
+        holder.nameTextView.setText(p.getName());
+
+        Glide.with(context)
+                .load("http://pokeapi.com/media/sprites/pokemon/" + p.getNumber() + ".png")
+                .centerCrop()
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.photoImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+    public void addPokemonList(ArrayList<Pokemon> pokemonList) {
+        dataset.addAll(pokemonList);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView photoImageView;
+        private TextView nameTextView;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            photoImageView = (ImageView) itemView.findViewById(R.id.photoImageView);
+            nameTextView = (TextView) itemView.findViewById(R.id.nameTextView);
+        }
+    }
+}
---
+++ app/src/main/java/com/grupp2/pokemon_app/models/Pokemon.java
@@ -0,0 +1,33 @@
+package com.grupp2.pokemon_app.models;
+
+public class Pokemon {
+
+    private int number;
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getNumber() {
+        String[] urlPart = url.split("/");
+        return Integer.parseInt(urlPart[urlPart.length - 1]);
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
